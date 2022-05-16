06c14bf72716de33e3dac58456037abb1e12dfdb
etienne-bip-boup <etienne.elgueder@gmail.com>
2022-05-16 17:45:29 +0200

    Added basic stats for the monsters

---
+++ monsters/monstersAttacks.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include "monstersAttacks.h"
+
+struct attack_ {
+
+};
+
+typedef struct attack_ * attack;
+
+void dab();
+
---
+++ monsters/monstersAttacks.h
@@ -0,0 +1,9 @@
+#ifndef THE_SPIRE_MONSTERSATTACKS_H
+#define THE_SPIRE_MONSTERSATTACKS_H
+
+struct attack_;
+
+typedef struct attack_ * attack;
+
+void dab();
+#endif //THE_SPIRE_MONSTERSATTACKS_H
