dffee6817abaf7d0cec9386bac2ff26843217e0b
marc_bayart <marc_bayart@ens.univ-artois.fr>
2022-05-16 17:39:58 +0200

    Main menu and credits added

--- main/main.c
+++ main/main.c
@@ -1,3 +1,45 @@
-void main(){
-    
+#include "main.h"
+
+void newGame(){
+    printf("\nWIP\n\n");
+    displayMainMenu();
+}
+
+void credits(){
+    printf("\nCrédits :\n");
+    printf("Projet de C basé sur le jeu 'Slay the Spire'.\n");
+    printf("Développé par Étienne EL GUEDER, Loïc CLEDELIN, César DUVAL et Marc BAYART.\n\n");
+    displayMainMenu();
+}
+
+void displayMainMenu(){
+    int choice = 0;
+    printf("1. Nouvelle partie\n");
+    printf("2. Crédits\n");
+    printf("3. Quitter\n");
+    printf("Que souhaitez-vous faire ? (entrez 1, 2 ou 3) ");
+    scanf("%d", &choice);
+
+    if (choice == 1)
+    {
+        newGame();
+    } else if (choice == 2)
+    {
+        credits();
+    } else if (choice == 3)
+    {
+        printf("\nMerci d'avoir joué !\n\n");
+        exit;
+    } else
+    {
+        printf("\nVous avez saisi un mauvais chiffre !\n\n");
+        displayMainMenu();
+    }
+}
+
+int main(){
+    printf("Bienvenue dans C the Spire !\n\n");
+    displayMainMenu();
+
+    return 1;
 }
