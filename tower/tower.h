50739588be215ba30adaeb8487ffa2bf90929c5c
HachimanKami <105297288+HachimanKami@users.noreply.github.com>
2022-06-13 15:24:04 +0200

    Include fonctionnel

--- tower/tower.h
+++ tower/tower.h
@@ -7,18 +7,26 @@
 
 #ifndef TOWER_TOWER_H
 #define TOWER_TOWER_H
-int nbEvent=5;
-/*
-struct room_;
-typedef struct room_* room;
 
-room createRoom(float m_floor,monster m_monster,int m_event);
-void createTowerBoucle(room previous1,room previous2,room previous3,room previous4);
+#include <stdlib.h>
+#include <time.h>
+#include "../monsters/monsters.h"
+
+
+struct room_;
+typedef struct room_ *room;
+
+room createRoom(float m_floor, monster m_monster, int m_event);
+
+void createTowerBoucle(room previous1, room previous2, room previous3, room previous4);
+
 room createTower();
+
 monster pickAMonster13();
+
 monster pickAMonster59();
+
 monster pickABoss();
 
 
-*/
 #endif //TOWER_TOWER_H
