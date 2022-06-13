e7eff639dc4933ba883bf7d4f0b26c66c63fad83
HachimanKami <105297288+HachimanKami@users.noreply.github.com>
2022-06-13 14:19:19 +0200

    Creation de la tour

---
+++ tower/tower.h
@@ -0,0 +1,24 @@
+//
+// Created by loicc on 08/06/2022.
+//
+
+
+
+
+#ifndef TOWER_TOWER_H
+#define TOWER_TOWER_H
+int nbEvent=5;
+/*
+struct room_;
+typedef struct room_* room;
+
+room createRoom(float m_floor,monster m_monster,int m_event);
+void createTowerBoucle(room previous1,room previous2,room previous3,room previous4);
+room createTower();
+monster pickAMonster13();
+monster pickAMonster59();
+monster pickABoss();
+
+
+*/
+#endif //TOWER_TOWER_H
